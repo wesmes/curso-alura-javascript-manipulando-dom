8dd82a825de0c050b67f7db4e4951a21ba8893ef
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-07-09 16:34:13 -0300

    Módulo 4 - Aula 3: Código à prova de falhas

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,17 +1,15 @@
-const subtrair = document.querySelector('#subtrair');
-const somar = document.querySelector('#somar');
 
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     });
 })
 
 function manipulaDados(operacao, controle){
 
-    const peca = controle.querySelector('.controle-contador');
+    const peca = controle.querySelector('[data-contador]');
 
     if(operacao === '-'){
         peca.value = parseInt(peca.value)-1
